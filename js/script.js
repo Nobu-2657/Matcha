77dd7bc19a451c5c215ac1378afaf3f762fb3965
yuto52 <221205013@ccmailg.meijo-u.ac.jp>
2023-07-06 15:19:20 +0900

    ページトップボタンを作成しました

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ window.addEventListener('load', function () {
         }
     });
 });
+
+/*$(function(){
+    var topButton=$('#pageTop');
+    topButton.hide();
+    
+    //ボタンの表示設定
+    $(window).scroll(function(){
+    if($(this).scrollTop()>80){
+        // 画面を80pxスクロールしたら、ボタンを表示する
+        topButton.fadeIn();
+        }else{
+        // 画面が80pxより上なら、ボタンを表示しない
+        topButton.fadeOut();
+        }
+    });
+    
+    // ボタンをクリックしたら、スクロールして上に戻る
+    topButton.click(function(){
+        $('body,html').animate({
+        scrollTop: 0},500);
+        return false;
+        });
+    });*/
